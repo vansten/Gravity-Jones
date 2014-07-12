053068e4d50d2227c085354cef4f1f960ca89283
Krzysztof Stengert <krzysztof.stengert@hotmail.com>
2014-07-13 00:38:14 +0200

    Kolce dzialaja

---
+++ Game/Assets/General/Scripts/Stinger.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Stinger : MonoBehaviour {
+
+    private float timer = 0.0f;
+    private float cooldown = 0.3f;
+    private GameObject other;
+    private bool dying = false;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if(dying)
+        {
+            timer += Time.deltaTime;
+            if(timer >= cooldown)
+            {
+                other.gameObject.SendMessage("Die");
+                dying = false;
+            }
+        }
+	}
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.tag == "Player" || col.gameObject.tag == "EnemyDyingBox")
+        {
+            other = col.gameObject;
+            dying = true;
+        }
+    }
+}
