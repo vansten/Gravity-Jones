faa9d539f7c09d3cc1716d96051c50af7162dc0f
Krzysztof Stengert <krzysztof.stengert@hotmail.com>
2014-08-09 12:23:35 +0200

    Added main menu. Waiting for graphic stuff. And I found a bug -> when a gamepad is unplugged and the keyboard is main controller the player is teleporting far far away on start :/

---
+++ Game/Assets/Scenes/MainMenu/Scripts/CreditsButton.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CreditsButton : MonoBehaviour {
+    
+    public GameObject CreditsSprite;
+
+    private bool mouseOver = false;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (mouseOver && Input.GetMouseButtonUp(0))
+        {
+            CreditsSprite.renderer.sortingLayerName = "Blood";
+        }
+	}
+
+    void OnMouseEnter()
+    {
+        this.GetComponent<SpriteRenderer>().color = Color.yellow;
+        mouseOver = true;
+    }
+
+    void OnMouseExit()
+    {
+        this.GetComponent<SpriteRenderer>().color = Color.green;
+        mouseOver = true;
+    }
+}
---
+++ Game/Assets/Scenes/Welcome Screen/Scripts/WelcomeScreen.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WelcomeScreen : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		PlayerPrefs.SetInt("Actual Level", 1);
+		PlayerPrefs.SetInt("Artifacts Count", 0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonUp(0) || Input.GetAxis("Trigger") < 0)
+		{
+			int actualLevel = PlayerPrefs.GetInt ("Actual Level");
+			if(actualLevel == 0){
+				Application.LoadLevel(1);
+			}
+			else Application.LoadLevel(actualLevel);
+		}
+	}
+}
